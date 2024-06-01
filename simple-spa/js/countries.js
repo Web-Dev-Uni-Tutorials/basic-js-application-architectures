351c5af3ae208d9fe663b51890b02c6d244ddcd7
Stu <31927084+yCodeTech@users.noreply.github.com>
2024-06-01 01:55:51 +0100

    Update countries.js solution
    Inline with the original singular repo code

--- simple-spa/js/countries.js
+++ simple-spa/js/countries.js
@@ -3,7 +3,7 @@ function loadData(url,callback)
 	fetch(url).then(function(response) {
 		return response.json();
 	}).then(function(json) {
-		callback(json); //this calls populateList(), it's a callback function
+		callback(json)
 	});
 } //end of loadData()
 
@@ -25,25 +25,46 @@ function createHandler(country)
 		populationEl.textContent = country.population;
 		listDiv.classList.add("hide");
 		detailsDiv.classList.remove("hide");
+		//store the current country's details in the history object
+		history.pushState(country,country.name,country.id);
 	}
-} 
+}
 
 function populateList(countries)
 {
-	const countriesList=document.querySelector("#countries-list");
+	const countriesFragment = document.createDocumentFragment();
 	countries.forEach(function(country){
 		const newLi=document.createElement("li");
 		newLi.textContent=country.name;
 		newLi.addEventListener("click", createHandler(country), false)
-		countriesList.appendChild(newLi);
+		countriesFragment.appendChild(newLi);
 	})
+	const countriesList=document.querySelector("#countries-list");
+	countriesList.appendChild(countriesFragment);
 }
-	
-	
 
 function goBack(){
 	listDiv.classList.remove("hide");
 	detailsDiv.classList.add("hide");
+	//add an empty history entry
+	history.pushState(null,null,"./");
+}
+
+//this function will be called when the browser back/forward button is hit
+function doHistory(evnt) {
+	if(evnt.state){
+		//show a countries's details
+		let country=evnt.state
+		titleEl.textContent = country.name;
+		capitalEl.textContent = country.capital;
+		populationEl.textContent = country.population;
+		listDiv.classList.add("hide");
+		detailsDiv.classList.remove("hide");
+	}else{
+		//show the list of all the countries
+		detailsDiv.classList.add("hide");
+		listDiv.classList.remove("hide");
+	}
 }
 
 function init(){
@@ -60,6 +81,10 @@ function init(){
 
 	//hide the details view on page load
 	detailsDiv.classList.add("hide");
+
+	//this event will be triggered when the browser back button is hit
+	window.addEventListener('popstate', doHistory,false);
+
 	//make fetch request
 	loadData("data/countries.json",populateList);
 }
